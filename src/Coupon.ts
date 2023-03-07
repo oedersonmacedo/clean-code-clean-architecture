7f6dbb8119220e4e1bcc0025234e2016b41ba124
Ederson Macedo <ederson.mos@gmail.com>
2023-03-07 18:28:25 -0300

    Create validation about date of expire in

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -1,5 +1,13 @@
 export default class Coupon {
-    constructor(readonly code: string, readonly percentage: number) {
+    constructor(readonly code: string, readonly percentage: number, readonly expireIn: Date) {
+        validateExpireIn(expireIn);
     }
+}
 
+function validateExpireIn(expireIn: Date) {
+    if(!expireIn) throw new Error('Expire In not informed');
+    expireIn = new Date(expireIn.getFullYear(), expireIn.getMonth(), expireIn.getDate(), 0, 0, 0, 0);
+    let today = new Date();
+    today = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0);
+    if(expireIn < today) throw new Error('Discount coupon has already expired');
 }
