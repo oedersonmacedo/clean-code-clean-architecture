f90c01cfe3cb5377d248e7f995947ed2558b4ed2
Ederson Macedo <ederson.mos@gmail.com>
2023-04-06 15:56:19 -0300

    Added expiration date validation behavior

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -5,7 +5,6 @@ export default class Coupon {
 }
 
 function validateExpireIn(expireIn: Date) {
-    if(!expireIn) throw new Error('Expire In not informed');
     expireIn = new Date(expireIn.getFullYear(), expireIn.getMonth(), expireIn.getDate(), 0, 0, 0, 0);
     let today = new Date();
     today = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0);
