8f9a51d56720eaba5c9afa73ab63299e2d8ee6c0
Ederson Macedo <ederson.mos@gmail.com>
2023-04-07 14:28:04 -0300

    created new tests for order

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -1,4 +1,5 @@
 import Coupon from "../src/Coupon";
+import Dimension from "../src/Dimension";
 import Item from "../src/Item";
 import Order from "../src/Order"
 
@@ -21,7 +22,34 @@ test('Should to create an order with discount coupon', function () {
     order.addItem(new Item(2, 'example 2', 'category 1', 5000), 3);
     order.addItem(new Item(3, 'example 3', 'category 2', 30), 4);
     const coupon = new Coupon('COUPON20', 20,  new Date());
-    order.addCoupon(coupon)
+    order.addCoupon(coupon);
 
     expect(order.getTotal()).toBe(13696);
+});
+
+test('Should to create an order with discount coupon expired', function () {
+    const order = new Order('111.444.777-35');
+    order.addItem(new Item(1, 'example 1', 'category 1', 1000), 2);
+    order.addItem(new Item(2, 'example 2', 'category 1', 5000), 3);
+    order.addItem(new Item(3, 'example 3', 'category 2', 30), 4);
+    const coupon = new Coupon('COUPON20', 20,  new Date('2020-01-01T00:00:00'));
+    order.addCoupon(coupon);
+
+    expect(order.getTotal()).toBe(17120);
+});
+
+test('Should to create an order with three items and freight', function () {
+    const order = new Order("935.411.347-80");
+	order.addItem(new Item(1, "example 1", "category", 1000, new Dimension(100, 30, 10), 3), 1);
+	order.addItem(new Item(2, "example 2", "category", 5000, new Dimension(100, 50, 50), 20), 1);
+	order.addItem(new Item(3, "example 3", "category", 30, new Dimension(10, 10, 10), 1), 3);
+	const total = order.getTotal();
+	expect(total).toBe(6350);
+});
+
+test('Should to create an order with minimum value of freight', function () {
+    const order = new Order("935.411.347-80");
+	order.addItem(new Item(1, "example 3", "category", 30, new Dimension(10, 10, 0.9), 1), 1);
+	const total = order.getTotal();
+	expect(total).toBe(40);
 });
