e5eac6adb697a283e586cb1e973a8cd88bb8e53c
Ederson Macedo <ederson.mos@gmail.com>
2023-04-07 12:50:12 -0300

    Created test for freight

---
+++ test/Freight.test.ts
@@ -0,0 +1,17 @@
+import Dimension from "../src/Dimension";
+import Freight from "../src/Freight";
+import Item from "../src/Item";
+
+test("Should to calculate the freight of an item", function () {
+	const item = new Item(1, "Example 1", "category", 1000, new Dimension(100, 30, 10), 3);
+	const freight = new Freight();
+	freight.addItem(item, 2);
+	expect(freight.getTotal()).toBe(60);
+});
+
+test("Should to calculate the freight min which is 10", function () {
+	const item = new Item(3, "Example 1", "category", 30, new Dimension(10, 10, 10), 0.9);
+	const freight = new Freight();
+	freight.addItem(item, 1);
+	expect(freight.getTotal()).toBe(10);
+});
