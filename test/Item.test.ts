a5420aecff91cac7e5b0a87128fa35d9644e437b
Ederson Macedo <ederson.mos@gmail.com>
2023-04-07 12:50:32 -0300

    Created test for Item

---
+++ test/Item.test.ts
@@ -0,0 +1,14 @@
+import Dimension from "../src/Dimension";
+import Item from "../src/Item";
+
+test("Should to create an item with dimension and to calculate the volume", function () {
+	const item = new Item(1, "example 1", "category 1", 1000, new Dimension(100, 30, 10));
+	const volume = item.getVolume();
+	expect(volume).toBe(0.03);
+});
+
+test("Should to create an item with dimension and to calculate the density", function () {
+	const item = new Item(1, "example 1", "category 1", 1000, new Dimension(100, 30, 10), 3);
+	const density = item.getDensity();
+	expect(density).toBe(100);
+});
