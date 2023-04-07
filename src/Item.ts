250d634c71bbc9d176141d8db1ee0838ff7706bb
Ederson Macedo <ederson.mos@gmail.com>
2023-04-07 12:46:10 -0300

    Created news aspects for dimensions and weight

--- src/Item.ts
+++ src/Item.ts
@@ -1,4 +1,17 @@
+import Dimension from "./Dimension";
+
 export default class Item {
-    constructor(readonly idItem: number, readonly description: string, readonly category: string, readonly price: number) {
+
+    constructor(readonly idItem: number, readonly description: string, readonly category: string, readonly price: number, readonly dimension?: Dimension, readonly weight?: number) {
+    }
+
+    getVolume() {
+        if (!this.dimension) return 0;
+        return this.dimension.getVolume();
+    }
+
+    getDensity() {
+        if(!this.weight || !this.dimension) return 0;
+        return this.weight / this.dimension.getVolume();
     }
 }
