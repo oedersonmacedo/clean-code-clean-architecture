d8d851807b4c757bf4cd5bab562534c67178729c
Ederson Macedo <ederson.mos@gmail.com>
2023-04-07 12:40:01 -0300

    Coupon refactored to have if expired

--- src/Coupon.ts
+++ src/Coupon.ts
@@ -1,12 +1,15 @@
 export default class Coupon {
-    constructor(readonly code: string, readonly percentage: number, readonly expireIn: Date) {
-        validateExpireIn(expireIn);
-    }
-}
 
-function validateExpireIn(expireIn: Date) {
-    expireIn = new Date(expireIn.getFullYear(), expireIn.getMonth(), expireIn.getDate(), 0, 0, 0, 0);
-    let today = new Date();
-    today = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0);
-    if(expireIn < today) throw new Error('Discount coupon has already expired');
+    constructor(readonly code: string, readonly percentage: number, readonly expireIn?: Date) {
+    }
+
+    isExpired (today: Date = new Date()) {
+        if (!this.expireIn) return false;
+        today = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0);
+        return this.expireIn < today;
+    }
+
+    calculateDiscount(amount: number) {
+        return (amount * this.percentage) / 100;
+    }
 }
