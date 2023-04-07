d2c653f0393edc5669f293de14dc447ccf2d70b1
Ederson Macedo <ederson.mos@gmail.com>
2023-04-07 12:40:44 -0300

    Test coupon refactored to have if expired

--- test/Coupon.test.ts
+++ test/Coupon.test.ts
@@ -1,17 +1,30 @@
 import Coupon from "../src/Coupon";
 
-test('Should not to generate Coupon with date yesteday', function () {
-    expect(() => {
-        const yesteday = new Date();
-        yesteday.setDate(yesteday.getDate() - 1);
-        new Coupon('CODE', 20, yesteday);
-    }).toThrow(new Error('Discount coupon has already expired'));
-});
-
 test('Should to generate Coupon', function () {
     const expireIn = new Date();
+    const coupon = new Coupon('CODE', 20);
+    expect(coupon.isExpired()).toBeFalsy();
+});
+
+test('Should to generate Coupon, and calculate discount', function () {
+    const expireIn = new Date();
     const coupon = new Coupon('CODE', 20, expireIn);
-    expect(coupon.code).toBe('CODE');
-    expect(coupon.percentage).toBe(20);
-    expect(coupon.expireIn).toBe(expireIn);
+    const discount = coupon.calculateDiscount(1000);
+    expect(coupon.isExpired()).toBeFalsy();
+});
+
+test('Should Coupon is expired with date yesteday, without today in isExpired', function () {
+    const yesteday = new Date();
+    yesteday.setDate(yesteday.getDate() - 1);
+    const coupon = new Coupon('CODE', 20, yesteday);
+    expect(coupon.isExpired()).toBeTruthy();
+});
+
+
+test('Should Coupon is expired, with today in isExpired', function () {
+    const today = new Date();
+    const coupon = new Coupon('CODE', 20, today);
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    expect(coupon.isExpired(tomorrow)).toBeTruthy();
 });
