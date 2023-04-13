68416243ee7733e1b2724a930f7f20d7ab8078ed
Ederson Macedo <ederson.mos@gmail.com>
2023-04-13 18:04:04 -0300

    Created simulate freight and test simulate freight

---
+++ test/integration/SimulateFreight.test.ts
@@ -0,0 +1,16 @@
+import SimulateFreight from "../../src/application/use-case/simulate-freight/SimulateFreight";
+import ItemRepositoryMemory from "../../src/infra/repository/memory/ItemRepositoryMamory";
+
+test("Should to simulate freight of an order", function() {
+    const itemRepository = new ItemRepositoryMemory();
+    const simulateFreight = new SimulateFreight(itemRepository);
+    const input = {
+        orderItems: [ 
+            { idItem: 1, quantity: 1},
+            { idItem: 2, quantity: 1},
+            { idItem: 3, quantity: 3}
+        ]
+    }
+    const output = simulateFreight.execute(input);
+    expect(output.total).toBe(260);
+})
