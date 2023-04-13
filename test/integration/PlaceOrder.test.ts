25d434f4f289a23936a2d1eba000d300e208c0b9
Ederson Macedo <ederson.mos@gmail.com>
2023-04-13 16:56:51 -0300

    Refactore and to create test in place order with the code

--- test/integration/PlaceOrder.test.ts
+++ test/integration/PlaceOrder.test.ts
@@ -1,27 +1,80 @@
-import { CouponRepositoryMemory } from "../../src/infra/repository/memory/CouponRepositoryMemory";
-import ItemRepositoryMemory from "../../src/infra/repository/memory/ItemRepositoryMamory";
-import { OrdemRepositoryMemory } from "../../src/infra/repository/memory/OrderRepositoryMamory";
 import PlaceOrder from "../../src/application/use-case/PlaceOrder";
-import PlaceOrderInput from "../../src/application/use-case/PlaceOrderInput";
+import CouponRepository from "../../src/domain/repository/CouponRepository";
+import ItemRepository from "../../src/domain/repository/ItemRepository";
+import OrderRepository from "../../src/domain/repository/OrderRepository";
+import CouponRepositoryMemory from "../../src/infra/repository/memory/CouponRepositoryMemory";
+import ItemRepositoryMemory from "../../src/infra/repository/memory/ItemRepositoryMamory";
+import OrderRepositoryMemory from "../../src/infra/repository/memory/OrderRepositoryMamory";
 
+let itemRepository: ItemRepository;
+let orderRepository: OrderRepository;
+let couponRepository: CouponRepository;
+
+beforeEach(function () {
+	itemRepository = new ItemRepositoryMemory();
+	orderRepository = new OrderRepositoryMemory();
+	couponRepository = new CouponRepositoryMemory();
+});
 test("Should to create a order", function () {
-    const itemRepository = new ItemRepositoryMemory();
-    const orderRepository = new OrdemRepositoryMemory();
-    const couponRepository = new CouponRepositoryMemory();
     const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
-    const orderItems = [ { idItem: 1, quantity: 1}, { idItem: 2, quantity: 1}, { idItem: 3, quantity: 3} ];
-    const input = new PlaceOrderInput("935.411.347-80", orderItems);
+    const input = {
+        cpf: "935.411.347-80",
+        orderItems: [ 
+            { idItem: 1, quantity: 1},
+            { idItem: 2, quantity: 1},
+            { idItem: 3, quantity: 3}
+        ],
+        issueDate: new Date("2023-04-13T10:00:00")
+    };
     const output = placeOrder.execute(input);
     expect(output.total).toBe(6090);
 })
 
-test("Should to create a order wiht coupon", function () {
-    const itemRepository = new ItemRepositoryMemory();
-    const orderRepository = new OrdemRepositoryMemory();
-    const couponRepository = new CouponRepositoryMemory();
+test("Should to create a order with coupon", function () {
     const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
-    const orderItems = [ { idItem: 1, quantity: 1}, { idItem: 2, quantity: 1}, { idItem: 3, quantity: 3} ];
-    const input = new PlaceOrderInput("935.411.347-80", orderItems, "COUPON20");
+    const input = {
+        cpf: "935.411.347-80",
+        orderItems: [ 
+            { idItem: 1, quantity: 1},
+            { idItem: 2, quantity: 1},
+            { idItem: 3, quantity: 3}
+        ],
+        coupon: "COUPON20",
+        issueDate: new Date("2023-04-13T10:00:00")
+    };
     const output = placeOrder.execute(input);
     expect(output.total).toBe(4872);
+})
+
+test("Should to create a order calculating a code", function () {
+    const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
+    const input = {
+        cpf: "935.411.347-80",
+        orderItems: [ 
+            { idItem: 1, quantity: 1},
+            { idItem: 2, quantity: 1},
+            { idItem: 3, quantity: 3}
+        ],
+        coupon: "COUPON20",
+        issueDate: new Date("2023-04-13T10:00:00")
+    };
+    const output = placeOrder.execute(input);
+    expect(output.code).toBe("202300000001");
+})
+
+test("Should to create a order calculating two code", function () {
+    const placeOrder = new PlaceOrder(itemRepository, orderRepository, couponRepository);
+    const input = {
+        cpf: "935.411.347-80",
+        orderItems: [ 
+            { idItem: 1, quantity: 1},
+            { idItem: 2, quantity: 1},
+            { idItem: 3, quantity: 3}
+        ],
+        coupon: "COUPON20",
+        issueDate: new Date("2023-04-13T10:00:00")
+    };
+    placeOrder.execute(input);
+    const output = placeOrder.execute(input);
+    expect(output.code).toBe("202300000002");
 })
