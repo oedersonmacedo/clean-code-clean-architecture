038643db1eb1214f081e318504e38cc2e713231c
Ederson Macedo <ederson.mos@gmail.com>
2023-04-14 12:34:22 -0300

    Created test repository database for the item

---
+++ test/integration/infra/database/ItemRepositoryDatabase.test.ts
@@ -0,0 +1,10 @@
+import PostgreSQLConnectionAdapter from "../../../../src/infra/database/PostgreSQLConnectionAdapter"
+import ItemRepositoryDatabase from "../../../../src/infra/repository/database/ItemRepositoryDatabase";
+
+test("Should to test the item repository", async function () {
+    const connection = new PostgreSQLConnectionAdapter();
+    const itemRepository = new ItemRepositoryDatabase(connection);
+    const item = await itemRepository.getById(1);
+    expect(item?.description).toBe("Guitarra");
+    connection.close();
+})
