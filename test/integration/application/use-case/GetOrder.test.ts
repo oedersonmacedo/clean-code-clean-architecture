ac546ebb000de590a3eb3d29f92f85f67bc67e6d
Ederson Macedo <ederson.mos@gmail.com>
2023-04-18 15:31:29 -0300

    Created new test of use case get order

---
+++ test/integration/application/use-case/GetOrder.test.ts
@@ -0,0 +1,38 @@
+import GetOrder from "../../../../src/application/use-case/get-order/GetOrder";
+import PlaceOrder from "../../../../src/application/use-case/place-order/PlaceOrder";
+import RepositoryFactory from "../../../../src/domain/factory/RepositoryFactory";
+import Connection from "../../../../src/infra/database/Connection";
+import PostgreSQLConnectionAdapter from "../../../../src/infra/database/PostgreSQLConnectionAdapter";
+import DatabaseRepositoryFactory from "../../../../src/infra/factory/DatabaseRepositoryFactory";
+
+let connection: Connection;
+let repositoryFactory: RepositoryFactory;
+
+beforeEach(async function () {
+	connection = new PostgreSQLConnectionAdapter();
+	repositoryFactory = new DatabaseRepositoryFactory(connection);
+	const orderRepository = repositoryFactory.createOrderRepository();
+	await orderRepository.clean();
+});
+
+test(" Should to get an order with the code", async function () {
+	const placeOrder = new PlaceOrder(repositoryFactory);
+	const input = {
+		cpf: "935.411.347-80",
+		orderItems: [
+			{ idItem: 1, quantity: 1},
+			{ idItem: 2, quantity: 1},
+			{ idItem: 3, quantity: 3}
+		],
+		coupon: "VALE20",
+		issueDate: new Date("2021-03-01T10:00:00")
+	};
+	await placeOrder.execute(input);
+	const getOrder = new GetOrder(repositoryFactory);
+	const output = await getOrder.execute("202100000001");
+	expect(output.total).toBe(5152);
+});
+
+afterEach(async function () {
+	await connection.close();
+});
