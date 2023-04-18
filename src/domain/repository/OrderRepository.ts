4c37b1092e8943348f18173e407171575a18391a
Ederson Macedo <ederson.mos@gmail.com>
2023-04-17 22:22:56 -0300

    Added news method for Order repository

--- src/domain/repository/OrderRepository.ts
+++ src/domain/repository/OrderRepository.ts
@@ -3,4 +3,8 @@ import Order from "../entity/Order";
 export default interface OrderRepository {
     count(): Promise<number>;
     save(order: Order): Promise<void>;
+    getByCode(code: string): Promise<Order>;
+    count(): Promise<number>;
+	getAll(): Promise<Order[]>;
+    clean(): Promise<void>;
 }
